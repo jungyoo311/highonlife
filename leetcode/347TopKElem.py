ff3ed43a788d350fe36566b8ef15ebc68ea30aa3
jung <jung@jungs-MacBook-Pro.local>
2025-03-27 14:48:54 -0700

    0327 347 minheap implementation

--- leetcode/347TopKElem.py
+++ leetcode/347TopKElem.py
@@ -18,4 +18,14 @@ class Solution:
         print(sorted_records)
         result = [pair[0] for pair in sorted_records[:k]] #practice this more.
         return result
-        #if using maxHeap, i can easily return the max 2 of the records list.
+        
+        #if using minHeap, i can easily return the max 2 of the records list.
+        # min heap implementations
+        import heapq
+        freq = {} #freq map
+        for num in nums:
+            if num in freq:
+                freq[num] += 1
+            else:
+                freq[num] = 1
+        result = heapq.nlargest(k, freq.keys(), key=lambda x: freq[x])
