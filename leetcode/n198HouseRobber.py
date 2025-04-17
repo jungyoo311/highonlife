e50505b13d760aef8c6707e654b1f87288f75741
jung <jung@jungs-MacBook-Pro.local>
2025-04-16 21:29:23 -0700

    fix house robber

--- leetcode/n198HouseRobber.py
+++ leetcode/n198HouseRobber.py
@@ -16,13 +16,13 @@ class Solution:
         #     r2 = temp
         # return r2
 
-        
+
         # SOL 2, using DP
         n = len(nums)
         dp = [-1] * n
         if n == 1:
             return nums[0]
-        dp[0], dp[1] = nums[0], nums[1]
+        dp[0], dp[1] = nums[0], max(nums[0], nums[1])
         # [1,2,3,1]
         for i in range(2, n):
             dp[i] = max(nums[i] + dp[i-2], dp[i-1])
